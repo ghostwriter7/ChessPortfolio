bad79c9c6f34493e6062d8e48c8e2b274f847501
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-06-10 10:14:26 +0200

    More functions for computing siblings done.

--- chess-logic/src/get-available-moves.ts
+++ chess-logic/src/get-available-moves.ts
@@ -48,27 +48,42 @@ export function getAvailableMoves(board: Board, selectedPosition: Position): Pos
         return availablePositions;
     }
 
+    if (name === 'bishop') {
+        // can move any number of cells diagonally (until there's a figure on its path)
+
+    }
 
 }
 
-const getLeftSibling = (position: Position): Position => {
+const isValidRowIndex = (index: number): boolean => index >= 1 && index <= 8;
+
+const getSibling = (position: Position, columnOffset: number, rowOffset: number): Position | null => {
     const columnId = position[0] as Letter;
     const columnIndex = letters.indexOf(columnId);
-    return `${letters[columnIndex - 1]}${position[1]}` as Position;
-}
-
-const getRightSibling = (position: Position): Position => {
-    const columnId = position[0] as Letter;
-    const columnIndex = letters.indexOf(columnId);
-    return `${letters[columnIndex + 1]}${position[1]}` as Position;
-}
-
-const getTopSibling = (position: Position): Position => {
     const rowIndex = parseInt(position[1]);
-    return `${position[0]}${rowIndex + 1}` as Position;
+
+    const newLetter = letters[columnIndex + columnOffset];
+    const newRowIndex = rowIndex + rowOffset;
+
+    if (newLetter && isValidRowIndex(newRowIndex)) {
+        return `${newLetter}${newRowIndex}` as Position;
+    }
+
+    return null;
 }
 
-const getBottomSibling = (position: Position): Position => {
-    const rowIndex = parseInt(position[1]);
-    return `${position[0]}${rowIndex - 1}` as Position;
-}
+const leftColumn = -1;
+const rightColumn = 1;
+const topRow = 1;
+const bottomRow = -1;
+const sameColumn = 0;
+const sameRow = 0;
+
+const getLeftSibling = (position: Position): Position | null => getSibling(position, leftColumn, sameRow);
+const getLeftTopSibling = (position: Position): Position | null => getSibling(position, leftColumn, topRow);
+const getLeftBottomSibling = (position: Position): Position | null => getSibling(position, leftColumn, bottomRow);
+const getRightTopSibling = (position: Position): Position | null => getSibling(position, rightColumn, topRow);
+const getRightBottomSibling = (position: Position): Position | null => getSibling(position, rightColumn, bottomRow);
+const getRightSibling = (position: Position): Position | null => getSibling(position, rightColumn, sameRow);
+const getTopSibling = (position: Position): Position | null => getSibling(position, sameColumn, topRow);
+const getBottomSibling = (position: Position): Position | null => getSibling(position, sameColumn, bottomRow);
