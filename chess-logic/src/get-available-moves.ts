4ca4b3a434a985c938db4281916609c0beb39221
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-06-10 10:23:22 +0200

    New abstraction created for walking the board.

--- chess-logic/src/get-available-moves.ts
+++ chess-logic/src/get-available-moves.ts
@@ -1,59 +1,8 @@
 import { Board } from "./types/board";
+import { FigureName } from "./types/figure-name";
 import { Letter, Position } from "./types/position";
 
 const letters: Letter[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
-export function getAvailableMoves(board: Board, selectedPosition: Position): Position[] {
-    const figure = board[selectedPosition];
-
-    if (!figure) return [];
-
-    const { color, name } = figure;
-    const isWhite = color === 'white';
-    const rowIndex = parseInt(selectedPosition[1]);
-    const columnId = selectedPosition[0] as Letter;
-    const columnIndex = letters.indexOf(columnId);
-
-    if (name === 'pawn') {
-        const availableColumnIndexes = [columnIndex - 1, columnIndex, columnIndex + 1]
-            .filter((index) => index >= 0 && index < 8);
-        const nextRowIndex = isWhite ? rowIndex + 1 : rowIndex - 1;
-
-        return availableColumnIndexes.map((columnIndex) =>
-            `${letters[columnIndex]}${nextRowIndex}` as Position);
-    }
-
-    if (name === 'rook') {
-        // can move any number of cells either horizontally or vertically (until there's a figure on its path)
-        const availablePositions: Position[] = [];
-
-        const walkBoard = (position: Position, nextPositionFn: (position: Position) => Position, path: Position[]): boolean => {
-            // occupied by another figure
-            if (board[position]) {
-                path.push(position);
-                return false;
-            }
-
-            // off the board
-            if (!board.hasOwnProperty(position)) return false;
-
-            return walkBoard(nextPositionFn(position), nextPositionFn, path);
-        }
-
-        walkBoard(selectedPosition, getTopSibling, availablePositions);
-        walkBoard(selectedPosition, getBottomSibling, availablePositions);
-        walkBoard(selectedPosition, getLeftSibling, availablePositions);
-        walkBoard(selectedPosition, getRightSibling, availablePositions);
-
-        return availablePositions;
-    }
-
-    if (name === 'bishop') {
-        // can move any number of cells diagonally (until there's a figure on its path)
-
-    }
-
-}
 
 const isValidRowIndex = (index: number): boolean => index >= 1 && index <= 8;
 
@@ -87,3 +36,59 @@ const getRightBottomSibling = (position: Position): Position | null => getSiblin
 const getRightSibling = (position: Position): Position | null => getSibling(position, rightColumn, sameRow);
 const getTopSibling = (position: Position): Position | null => getSibling(position, sameColumn, topRow);
 const getBottomSibling = (position: Position): Position | null => getSibling(position, sameColumn, bottomRow);
+
+const directions: Partial<Record<FigureName, ((position: Position) => Position | null)[]>> = {
+    bishop: [getLeftTopSibling, getLeftBottomSibling, getRightBottomSibling, getRightTopSibling],
+    rook: [getLeftSibling, getBottomSibling, getRightSibling, getTopSibling]
+}
+
+const walkBoard = (board: Board, position: Position, nextPositionFn: (position: Position) => Position, path: Position[]): void => {
+    if (!position) return;
+
+    if (board[position]) {
+        path.push(position);
+        return;
+    }
+
+    path.push(position);
+    walkBoard(board, nextPositionFn(position), nextPositionFn, path);
+}
+
+const getAvailablePositions = (board: Board, name: FigureName, position: Position): Position[] => {
+    const availablePositions: Position[] = [];
+
+    directions[name].forEach((directionFn) => {
+       walkBoard(board, directionFn(position), directionFn, availablePositions);
+    });
+
+    return availablePositions;
+}
+
+export function getAvailableMoves(board: Board, selectedPosition: Position): Position[] {
+    const figure = board[selectedPosition];
+
+    if (!figure) return [];
+
+    const { color, name } = figure;
+    const isWhite = color === 'white';
+    const rowIndex = parseInt(selectedPosition[1]);
+    const columnId = selectedPosition[0] as Letter;
+    const columnIndex = letters.indexOf(columnId);
+
+    if (name === 'pawn') {
+        const availableColumnIndexes = [columnIndex - 1, columnIndex, columnIndex + 1]
+            .filter((index) => index >= 0 && index < 8);
+        const nextRowIndex = isWhite ? rowIndex + 1 : rowIndex - 1;
+
+        return availableColumnIndexes.map((columnIndex) =>
+            `${letters[columnIndex]}${nextRowIndex}` as Position);
+    }
+
+    if (name === 'rook') {
+        return getAvailablePositions(board, name, selectedPosition);
+    }
+
+    if (name === 'bishop') {
+        return getAvailablePositions(board, name, selectedPosition);
+    }
+}
