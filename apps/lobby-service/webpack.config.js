3fdeaff343ac2d5576c119039b2115a636d08615
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-06-27 09:07:51 +0200

    LobbySrv generated, added to Docker

---
+++ apps/lobby-service/webpack.config.js
@@ -0,0 +1,22 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  output: {
+    path: join(__dirname, '../../dist/apps/lobby-service'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: [],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+      sourceMap: true,
+    }),
+  ],
+};
