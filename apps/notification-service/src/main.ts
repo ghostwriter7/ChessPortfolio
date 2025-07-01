d86d6e0db81528fb421f336b149d52195046eaa2
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-07-01 13:09:42 +0200

    SubscriberService utilised in NotificationService

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,13 +1,11 @@
 import {
+  createSubscriberService,
   EMAIL_NOTIFICATION_REQUESTED,
   EventToPayload,
   loggerFactory,
-  retry,
 } from '@api';
-import { Kafka } from 'kafkajs';
 import * as nodemailer from 'nodemailer';
 
-const BROKER_PORT = process.env.BROKER_PORT;
 const EMAIL_ACCOUNT = process.env.EMAIL_ACCOUNT;
 const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 
@@ -26,46 +24,33 @@ const transporter = nodemailer.createTransport({
 
 const logger = loggerFactory();
 
-const initialize = async () => {
-  const kafka = new Kafka({
-    clientId: 'notification-srv',
-    brokers: [`broker:${BROKER_PORT}`],
+try {
+  const subscriberService = await createSubscriberService();
+  await subscriberService.subscribe(EMAIL_NOTIFICATION_REQUESTED);
+  await subscriberService.run(async ({ topic, partition, message }) => {
+    logger.info(`Received message on ${topic} [${partition}]`);
+    try {
+      const {
+        email,
+        subject,
+        message: emailMessage,
+      } = (JSON.parse(
+        message.value.toString()
+      ) as EventToPayload['EMAIL_NOTIFICATION_REQUESTED']) || {};
+
+      const info = await transporter.sendMail({
+        from: EMAIL_ACCOUNT,
+        subject,
+        to: email,
+        html: emailMessage,
+      });
+
+      logger.info(`Message sent: ${info.messageId}`);
+    } catch (error) {
+      logger.error('Failed to send an e-mail');
+      console.error(error);
+    }
   });
-
-  const consumer = kafka.consumer({ groupId: 'notification-srv' });
-
-  await consumer.connect();
-
-  await consumer.subscribe({
-    topic: EMAIL_NOTIFICATION_REQUESTED,
-  });
-
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      logger.info(`Received message on ${topic} [${partition}]`);
-      try {
-        const {
-          email,
-          subject,
-          message: emailMessage,
-        } = (JSON.parse(
-          message.value.toString()
-        ) as EventToPayload['EMAIL_NOTIFICATION_REQUESTED']) || {};
-
-        const info = await transporter.sendMail({
-          from: EMAIL_ACCOUNT,
-          subject,
-          to: email,
-          html: emailMessage,
-        });
-
-        logger.info(`Message sent: ${info.messageId}`);
-      } catch (error) {
-        logger.error('Failed to send an e-mail');
-        console.error(error);
-      }
-    },
-  });
-};
-
-retry(initialize, logger);
+} catch (e) {
+  logger.error('Notification-Service failed to start', e);
+}
