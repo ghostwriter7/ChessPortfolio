056178399468aed66755d7114b810863bdb19f36
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-07-01 14:39:05 +0200

    Types in PlayerRepo improved

--- libs/api/src/repository/player.repository.ts
+++ libs/api/src/repository/player.repository.ts
@@ -1,21 +1,22 @@
 import { Socket } from 'socket.io';
+import { Username } from '../auth/username.type';
 
 export class PlayerRepository {
-  private readonly usernameToSocketMap = new Map<string, Socket>();
+  private readonly usernameToSocketMap = new Map<Username, Socket>();
 
-  public add(username: string, socket: Socket): void {
+  public add(username: Username, socket: Socket): void {
     this.usernameToSocketMap.set(username, socket);
   }
 
-  public getSocketByUsername(username: string): Socket | undefined {
+  public getSocketByUsername(username: Username): Socket | undefined {
     return this.usernameToSocketMap.get(username);
   }
 
-  public getUsernames(): string[] {
+  public getUsernames(): Username[] {
     return this.usernameToSocketMap.keys().toArray();
   }
 
-  public deleteByUsername(username: string): void {
+  public deleteByUsername(username: Username): void {
     this.usernameToSocketMap.delete(username);
   }
 }
